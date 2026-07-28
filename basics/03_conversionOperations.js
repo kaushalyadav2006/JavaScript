621eee1fdf1ee23ec11fc608a66f8eb43d2f9686
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-07-28 19:54:13 +0530

    conversion and opertions

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -2,7 +2,7 @@
 // let score = null
 // let score = undefined
 // let score = true
-let score = "kaushal"
+// let score = "kaushal"
 
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -31,7 +31,51 @@ let score = "kaushal"
 
 
 
-let someNumber = 3728
-let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber = 3728
+// let StringNumber = String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+//************************************** Operations ************************** */
+
+let value = 3
+
+let negValu = -value
+// console.log(negValu);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+// let str1 = "hello"
+// let str2 = " Kaushal"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2= num3 =  2 + 2
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
