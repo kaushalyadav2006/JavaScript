baec1f9e86e0f35418240cda2efa10a1bb0428b1
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-07-28 19:17:24 +0530

    conversion of datatypes

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+// let score = "35abcs"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "kaushal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true >=1 ; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = "kaushal"
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+
+let someNumber = 3728
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
