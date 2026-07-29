8e3f1f6581b903a2f012f13a8d5d03f9d27acf31
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-07-29 17:11:31 +0530

    array part 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,49 @@
+// arrays
+
+const myArray = [0,1,2,3,4,5];
+const Alphabets = ["apple", "ball", "cat", "dog"];
+
+const myArr2 = new Array(0,1,2,3,4,5);
+// console.log(myArr2[3]);
+
+
+//array methods
+// myArr2.push(5);
+// myArr2.push(6);
+// myArr2.push(7);
+
+// myArr2.pop()
+
+// myArr2.unshift(9);
+// myArr2.shift();
+
+// console.log(myArr2.includes(3));
+// console.log(myArr2.indexOf(3));
+
+const newArr = myArr2.join();
+
+// console.log(myArr2);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+//slice and splice
+console.log("A ", myArr2);
+
+const myn1 = myArr2.slice(1,3);
+
+console.log(myn1)
+console.log("B ", myArr2);
+
+const myn2 = myArr2.splice(1,3);
+console.log("C ", myArr2);
+console.log(myn2)
+
+// console.log("C ", myArr2);
+
+/**
+  The primary difference is that slice() does not change the
+  original array, while splice() mutates (changes) the original array. 
+  Think of slice() as taking a picture of a piece of cake (leaves it intact), 
+  and splice() as physically cutting and replacing pieces of that cake.
+ */
